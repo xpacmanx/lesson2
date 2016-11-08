fc24567b313c93b5332ba35ac93f673f37a76259
ipacmanx <ipacmanx@gmail.com>
2016-11-08 11:42:52 +0300

    fix

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 var express = require('express');
 var app = express();
+
+app.get('/', function (req, res) {
+  var result = 0;
+  if (undefined != req.query.a && undefined != req.query.b) {
+        result = parseInt(req.query.a) + parseInt(req.query.b);
+  }
+  res.send(200,result);
+});
+
+app.listen(3000);
